3a401a22e24c9756e41947fce5cc14b190337a11
samina Suppra <saminasuppra.phd@gmail.com>
2025-04-26 13:05:10 +0500

    Add files via upload

---
+++ firstfile.py
@@ -0,0 +1,8 @@
+studentname="Samina Suppra"
+serialnumber="0002"
+adress="House no. 34 A, Paradise city, Gojra road, Jhang."
+phonenumber="03338497754"
+print(studentname)
+print(serialnumber)
+print(adress)
+print(phonenumber)
