4fc049139c74fcbc3f6e37cbbb4e9a9c5c79a250
samina Suppra <saminasuppra.phd@gmail.com>
2025-05-07 15:15:49 +0500

    Add files via upload

---
+++ list_dictionary_practise.py
@@ -0,0 +1,14 @@
+fruits=["Kiwi", "black berry", "pear"]
+#Display all item in list
+print("fruits_list:", fruits)
+print("second_fruit:",fruits[1] )
+fruits.append("banana")
+print("updated_list:", fruits)
+Student= {"name" : "samina_suppra", 
+          "age": "37" , 
+          "grade":"22"
+          }
+print("student_detail:", Student)
+print("Name:",Student["name"])
+Student["School"]= "Lincoln_University_College"
+print("updated_student_info:", Student)
